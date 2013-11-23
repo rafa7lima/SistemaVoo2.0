daef63859fe11094a76625cbc6b968bbedda4e7a
iago8 <iago8mendes@hotmail.com>
2013-11-23 18:54:55 -0200

    caso de teste para pacotes infra,util
    ainda falta colocar recheio no codigo

---
+++ src/sistemaVoo/infraestrutura/teste/VooTeste.java
@@ -0,0 +1,24 @@
+package sistemaVoo.infraestrutura.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VooTeste {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
